a35470223672c03d5ee7df512c9994ebf02bd5e0
LiamLin <liamlin5609@gmail.com>
2024-05-20 16:39:53 -0400

    update

---
+++ App1/bonus3.py
@@ -0,0 +1,4 @@
+meals = ['pizza', 'pasta', 'salad']
+
+for meal in meals:
+    print(meal.capitalize())
--- App1/main.py
+++ App1/main.py
@@ -1,17 +1,26 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, or exit: ")
+    user_action = input("Type add, show, edit, or exit: ")
+    user_action = user_action.strip()
 
     match user_action:
         case 'add':
             todo = input("Enter a ToDo: ")
-            todos.append(todo.title())
-        case 'show':
+            todos.append(todo)
+        case 'show' | 'display':
             for item in todos:
+                item = item.title()
                 print(item)
         case 'exit':
             break
+        case 'edit':
+            number = input("Number of the ToDo to edit: ")
+            existing_todo = todos[int(number) - 1]
+            new_todo = input("Enter a new ToDo: ")
+            todos[int(number) - 1] = new_todo
+        case whatever:
+            print('You entered an unknown command!!')
 
 print('Bye!')
 
