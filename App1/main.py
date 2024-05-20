51825c13c15a4833cef3a3bef4dafb5b217c65f6
LiamLin <liamlin5609@gmail.com>
2024-05-19 23:27:38 -0400

    update

--- App1/main.py
+++ App1/main.py
@@ -1,2 +1,8 @@
-user_text = input("Plz enter a todo: ")
-print(user_text)
+to_do1 = input("Plz enter a todo: ")
+to_do2 = input("Plz enter a todo: ")
+to_do3 = input("Plz enter a todo: ")
+
+to_dos = [to_do1, to_do2, to_do3]
+print(to_dos)
+
+print(type(to_dos))
