0899cf6d989187178c55f648d1372b27971a3071
LiamLin <liamlin5609@gmail.com>
2024-06-01 14:01:04 -0400

    update

--- App1/bonus/bonus15.py
+++ App1/bonus/bonus15.py
@@ -1,23 +1,29 @@
 import FreeSimpleGUI as sg
+from zip_creator import make_archive
 
 label1 = sg.Text("Select files to compress: ")
 input1 = sg.Input()
-choose_button1 = sg.FileBrowse("Choose", key="files")
+choose_button1 = sg.FilesBrowse("Choose", key="files")
 
 label2 = sg.Text("Select destination folder: ")
 input2 = sg.Input()
 choose_button2 = sg.FolderBrowse("Choose", key="folder")
 
 button3 = sg.Button("Compress")
+output_label = sg.Text(key="output", text_color="yellow")
 
 window = sg.Window("File Compressor",
                    layout=[[label1, input1, choose_button1],
                            [label2, input2, choose_button2],
-                           [button3]])
+                           [button3, output_label]])
 
 while True:
     event, values = window.read()
     print(event, values)
     filepath = values['files'].split(";")
     folder = values['folder']
+    make_archive(filepath, folder)
+    window['output'].update(value="Compression Completed!!")
+
+
 window.close()
--- App1/bonus/zip_creator.py
+++ App1/bonus/zip_creator.py
@@ -6,7 +6,8 @@ def make_archive(filepaths, dest_dir):
     dest_path = pathlib.Path(dest_dir, 'compressed.zip')
     with zipfile.ZipFile(dest_path, 'w') as archive:
         for filepath in filepaths:
-            archive.write(filepath)
+            filepath = pathlib.Path(filepath)
+            archive.write(filepath, arcname=filepath.name)
 
 
 if __name__=="__main__":
